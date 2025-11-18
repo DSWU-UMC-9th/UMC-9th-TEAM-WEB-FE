71c1f1fd3cb9795ae32a0ff0ce510ae4c8a35c04
jinhyo <kjhyo0220@gmail.com>
2025-11-18 13:18:17 +0900

    🎉 Init: 프로젝트 세팅 (#1)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './App.css';
+
+export const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
