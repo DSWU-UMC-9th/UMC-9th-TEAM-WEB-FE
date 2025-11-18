b10c60a841781959518543aaeea14ca78e4d58b0
jinhyo <kjhyo0220@gmail.com>
2025-11-18 14:40:09 +0900

    ✨ Feat: 폴더 추가 세팅 및 devtools 위치 변경 (#1)
    플로팅 버튼과 겹치지 않도록 좌측 하단으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
+      <ReactQueryDevtools buttonPosition="bottom-left" />
     </QueryClientProvider>
   );
 }
