ef52172039b9e87d51ff2dc5fe9ac7d7336ffd2c
jinhyo <kjhyo0220@gmail.com>
2025-11-18 14:39:32 +0900

    ✨ Feat: ScrollToTop 설정 (#1)

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '@/components/Navbar/Navbar';
+import Navbar from '@/components/Navbar';
+import ScrollToTop from '@/utils/ScrollToTop';
 
 const HomeLayout = () => {
   return (
     <div className="flex flex-col h-dvh">
+      <ScrollToTop />
       <Navbar />
 
       <div className="flex-1">
