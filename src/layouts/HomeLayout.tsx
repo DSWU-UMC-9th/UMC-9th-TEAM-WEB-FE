0680399ddf574f36b9c5e9432c957e4c466efbce
jinhyo <kjhyo0220@gmail.com>
2025-11-25 17:20:02 +0900

    ✨ Feat: 플로팅 버튼 퍼블리싱 (#15)

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '@/components/Navbar';
 import ScrollToTop from '@/utils/ScrollToTop';
+import FloatingButton from '@/components/FloatingButton';
 
 const HomeLayout = () => {
   return (
@@ -11,6 +12,8 @@ const HomeLayout = () => {
       <div className="flex-1">
         <Outlet />
       </div>
+
+      <FloatingButton />
     </div>
   );
 };
